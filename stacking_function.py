ba8d7d6298a8dd2cd8cfb0c83710c25654014f54
Freek2002Verloop <144881596+Freek2002Verloop@users.noreply.github.com>
2023-11-30 09:47:22 +0100

    Update stacking_function.py

--- stacking_function.py
+++ stacking_function.py
@@ -1,20 +1,30 @@
-from HUI import HUI
+"""
+measurements:               [LxWxH]
+small box measurements :    [400x205x85]
+medium box measurements:    [400x205x125]
+large box measurements:     [400x205x165]
+extra large measurements:   [420x260x260]
+"""
 
-class Stacking_boxes:
+class Stacking_boxes():
     def __init__(self):
-        self.height_small_box = 1
-        self.height_medium_box = 2
-        self.height_large_box = 3
-        self.max_height_pallet = 2
+        self.height_small_box = 85
+        self.height_medium_box = 125
+        self.height_large_box = 165
+        self.height_extralarge_box = 260
+        self.max_height_pallet = 1000
         self.stacking_matrix = [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0]
-        ]
-        self.box_counts = {'small': 0, 'medium': 0, 'large': 0}
-        self.Smallest_stack()
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0]
+                                ]
+        self.box_counts = {'small': 0, 'medium': 0, 'large': 0, 'extra large': 0}
 
     def boxes(self, box_size):
-        sizes = {'small': self.height_small_box, 'medium': self.height_medium_box, 'large': self.height_large_box}
+        sizes = {'small': self.height_small_box,
+                'medium': self.height_medium_box,
+                'large': self.height_large_box,
+                'extra large': self.height_extralarge_box}
+        
         if box_size in sizes:
             return sizes.get(box_size)
         return 0
@@ -30,28 +40,40 @@ class Stacking_boxes:
 
         return self.stacking_matrix
 
-    def Smallest_stack(self):
+    def Smallest_stack(self, Input_val= "" ):
         while any(val <= self.max_height_pallet for row in self.stacking_matrix for val in row):
-            box_size = input('Fill in size (small/medium/large): ')
+            box_size = input(f'Fill in size (small/medium/large): ')
             box_size_val = self.boxes(box_size)
             print(f'Size: {box_size_val}')
             self.stacking_matrix = self.stack(box_size_val)
             print(self.stacking_matrix)
             if box_size_val > 0:
                 self.update_counts(box_size)
+            self.show_box_counts()
     
     def update_counts(self, box_size):
+        Box_list = [0,0,0,0]
         if box_size == 'small':
             self.box_counts['small'] += 1
+            Box_list[0]+=1
         elif box_size == 'medium':
             self.box_counts['medium'] += 1
+            Box_list[1]+=1
         elif box_size == 'large':
             self.box_counts['large'] += 1
+            Box_list[2]+=1
+        elif box_size == 'extra large':
+            self.box_counts['extra large'] += 1
+            Box_list[3]+=1
+        return Box_list
 
     def show_box_counts(self):
         print("Box counts:")
         for box_size, count in self.box_counts.items():
             print(f"{box_size.capitalize()} boxes: {count}")
 
-main = Stacking_boxes()
-main.show_box_counts()
+if __name__ == '__main__':
+    stacker = Stacking_boxes()
+    stacker.Smallest_stack()
+else: 
+    Stacking_boxes()
