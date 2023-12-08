9c154b0534263f9029b9e16ec3c6740f76fc0c27
Freek2002Verloop <144881596+Freek2002Verloop@users.noreply.github.com>
2023-12-08 11:59:39 +0100

    Create Storage.py

---
+++ Storage.py
@@ -0,0 +1,48 @@
+from Randomsize import Random_size as RS
+import Randomsize as Rdm
+import time
+
+def fill_buf():
+    for i in range (0,buf_size):
+        size = RS()
+        Buf.append(size)
+        print(Buf)
+
+def refill_buf():
+    while True:
+        time.sleep(0.1)
+        while len(Buf) < buf_size:
+            Buf.append(RS())
+        return Buf
+
+def take_buf():
+    count_list = []
+    count_list.append(Buf.count('s')), count_list.append(Buf.count('m')), count_list.append(Buf.count('l')), count_list.append(Buf.count('xl'))
+    print(count_list)
+    for i in range(4):
+        if count_list[i] >= 4 or count_list[3] >= 3:
+            if i == 0 : take_size = 's'
+            if i == 1 : take_size = 'm'
+            if i == 2 : take_size = 'l'
+            if i == 3 : take_size = 'xl'
+    time.sleep(0.1)
+    if take_size in Rdm.sizes:
+        if take_size != 'xl':
+            for i in range(0,4):                                  
+                Buf.remove(take_size)
+                refill_buf()
+                print(Buf)
+        else:
+            for i in range(0,3):                                  
+                Buf.remove(take_size)
+                refill_buf()
+                print(Buf)
+    return Buf, take_size
+
+buf_size = 12   
+Buf = []
+
+if __name__ == '__main__':
+    fill_buf()
+    for i in range (10):
+        take_buf()
