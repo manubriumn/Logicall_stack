ec9e882374ff6028f5a6131db56f526547aa36a6
ibnu haafizha yoga <103341906+manubriumn@users.noreply.github.com>
2023-12-18 10:38:42 +0100

    Update and rename Randomsize.py to Randomsize_b.py

--- Randomsize_b.py
+++ Randomsize_b.py
@@ -1,6 +1,8 @@
 import random
 
-sizes = ['s', 'm', 'l', 'xl']
+sizes = ['s', 's','s']
 def Random_size():
     chosen_size = random.choice(sizes)
     return chosen_size
+if __name__ == '__main__':
+    Random_size()
