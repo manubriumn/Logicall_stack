8f0d7753219553a1049dd1273d4c1891d332096a
ibnu haafizha yoga <103341906+manubriumn@users.noreply.github.com>
2023-12-20 11:57:21 +0100

    Update and rename Storage_b.py to Storage_c.py

--- Storage_c.py
+++ Storage_c.py
@@ -3,8 +3,8 @@ import randomsize as Rdm
 import time
 
 def fill_buf():
-    if len(Buf)< buf_size:
-        for i in range (0,buf_size):
+    if len(Buf) < buf_size:
+        for i in range(len(Buf), buf_size):
             size = RS()
             Buf.append(size)
             print(Buf)
@@ -22,27 +22,32 @@ def take_buf(Buf):
     print(Buf)
     count_list.append(Buf.count('s')), count_list.append(Buf.count('m')), count_list.append(Buf.count('l')), count_list.append(Buf.count('xl'))
     print(count_list)
-    for i in range(4):
-        if count_list[i] >= 4 or count_list[3] >= 3:
-            if i == 0 : take_size = 's'
-            if i == 1 : take_size = 'm'
-            if i == 2 : take_size = 'l'
-            if i == 3 : take_size = 'xl'
+
+    # Find the index of the maximum count
+    max_count_index = count_list.index(max(count_list))
+
+    take_size = ''
+    if count_list[max_count_index] >= 4 or count_list[3] >= 3:
+        if max_count_index == 0: take_size = 's'
+        elif max_count_index == 1: take_size = 'm'
+        elif max_count_index == 2: take_size = 'l'
+        elif max_count_index == 3: take_size = 'xl'
+
     time.sleep(0.1)
     if take_size in Rdm.sizes:
         if take_size != 'xl':
-            for i in range(0,4):
+            for i in range(4):
+                # Remove only one occurrence at a time
                 Buf.remove(take_size)
-                refill_buf()
                 print(Buf)
         else:
-            for i in range(0,3):
+            for i in range(3):
+                # Remove only one occurrence at a time
                 Buf.remove(take_size)
-                refill_buf()
                 print(Buf)
     return Buf, take_size
 
-buf_size = 9
+buf_size = 12
 Buf = []
 if __name__ == '__main__':
     fill_buf()
