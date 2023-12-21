554e08950a2677d9750c0401fbf92354bc7260c2
Freek2002Verloop <144881596+Freek2002Verloop@users.noreply.github.com>
2023-12-21 14:37:49 +0100

    Update Randomsize_d.py
    added the amount of boxes

--- Randomsize_c.py
+++ Randomsize_c.py
@@ -1,8 +1,21 @@
 import random
 
-sizes = ['s', 'm', 'l', 'xl']
+sizes = []
+size = ['s', 'm', 'l', 'xl']
 def Random_size():
+    Fill_size()
     chosen_size = random.choice(sizes)
     return chosen_size
-if __name__ == '__main__':
-    Random_size()
+
+def Fill_size():
+    for i in range(14):
+        sizes.append(size[0])
+        sizes.append(size[1])
+    for i in range(10):
+        sizes.append(size[2])
+    for i in range(6):
+        sizes.append(size[3])
+    print(sizes)
+
+if __name__== '__main__':
+    print(Random_size())
