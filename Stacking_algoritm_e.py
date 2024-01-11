5ab93e96078a06b1281051d07d8a3079cbf7fabc
ibnu haafizha yoga <103341906+manubriumn@users.noreply.github.com>
2024-01-11 10:31:12 +0100

    Update and rename Stacking_algoritm_d.py to Stacking_algoritm_e.py

--- Stacking_algoritm_e.py
+++ Stacking_algoritm_e.py
@@ -1,12 +1,14 @@
 import storage as ST
 import time
-matrix = [-420, -420]
-
-L_coords1 = [(-965, -185), (-965, 81), (-965, 350)]
+matrix = [-420]
+L_coords1 = [(-755, -185),(-755, 81),(-755, 350)]
+# L_coords1 = [(-965, -185), (-965, 81), (-965, 350)]
 L_coords2 = [(-545, -185), (-545, 81), (-545, 350)]
-coords1 = [(-1065, -128), (-863, -128),(-1065, 270), (-863, 270)]
+#coords1 = [(-1065, -128), (-863, -128),(-1065, 270), (-863, 270)]
+coords1 =  [(-863, -128),(-648, -128),(-863, 270),(-648, 280)]
 coords2 = [(-648, -128), (-443, -128),(-648, 280), (-443, 280)]
 coords3 = [(-230, 280), (-230, 280), (-230, -128), (-230, -128)]
+
 
 def last_matrix(matrix):
     old_matrix = matrix
@@ -20,14 +22,14 @@ def place(size):
     height = height_size[size]
 
     if size == 'xl':
-        if matrix[0] <= matrix[1]:  # Ensure index 1 doesn't exceed index 0
-            matrix[0] += height
-            coords_val[0] = True
-        else:
-            extra_height = matrix[1] + height - matrix[0]
-            matrix[0] += height + extra_height
-            matrix[1] += extra_height
-            coords_val[1] = True
+        # if matrix[0] <= matrix[1]:  # Ensure index 1 doesn't exceed index 0
+        matrix[0] += height
+        coords_val[0] = True
+        # else:
+        #     extra_height = matrix[1] + height - matrix[0]
+        #     matrix[0] += height + extra_height
+        #     matrix[1] += extra_height
+        #     coords_val[1] = True
     else:
         smallest = matrix.index(min(matrix))
         if smallest != 2:
@@ -46,9 +48,9 @@ def coordinates(Coords_val, size, Height):
     if size == 'xl' and index == 0:
         L_coords_1 = [(x[0], x[1], matrix[0]) for x in L_coords1]
         return L_coords_1
-    elif size == 'xl' and index == 1:
-        L_coords_2 = [(x[0], x[1], matrix[1]) for x in L_coords2]
-        return L_coords_2
+    # elif size == 'xl' and index == 1:
+    #     L_coords_2 = [(x[0], x[1], matrix[1]) for x in L_coords2]
+    #     return L_coords_2
     elif size != 'xl' and index == 0:
         coords_1 = [(x[0], x[1], matrix[0]) for x in coords1]
         return coords_1
@@ -76,7 +78,7 @@ def give_coords(coords):
 
 
 if __name__ == '__main__':
-    time.sleep(3)
+    time.sleep(.1)
     print("start")
     _, size = ST.take_buf(ST.Buf)
     x, Coords_val, Height = place()
